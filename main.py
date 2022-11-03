6faf697821e7c75b1201911ff10281edde7ab579
Oskreduard <oscar.avila.ing@gmail.com>
2022-11-03 17:53:26 -0500

    Iniciando Servidor para API

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from waitress import serve
+
+app=Flask(__name__)
+cors = CORS(app)
+
+@app.route("/",methods=['GET'])
+def test():
+    json = {}
+    json["message"] = "Server running mision tic 2022 final..."
+    return jsonify(json)
+
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+
+if __name__=='__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
